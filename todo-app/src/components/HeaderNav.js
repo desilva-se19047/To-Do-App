6b36a03f59d28d2c0cfc550cca396c00bd4ab3d1
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-10-09 19:42:46 +0530

    add header css

--- todo-app/src/components/HeaderNav.js
+++ todo-app/src/components/HeaderNav.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './ComponentStyle.css';
+import notification from "../assets/Notifications.svg";
+import profile from "../assets/Profile.svg";
+import chevron from "../assets/Chevron-down.svg";
 
 const Header = () => {
   return (
     <header className="header">
-      <h1>My Simple Header</h1>
-      
+        <div></div>
+        <div>
+            <h4>Dashboard</h4>
+        </div>
+        <div>
+            <img src= {notification} alt="Notification" className="header-image-1" />
+            <img src= {profile} alt="Profile" className="header-image-2" />
+            <img src= {chevron} alt="Chevron-down" className="header-image-3" />
+        </div>
+        <div className="divider"></div>  
     </header>
   );
 };
