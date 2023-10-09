fae2c8dd80732fe3b8e0828cc566447e22268883
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-10-09 17:21:18 +0530

    add header

---
+++ todo-app/src/components/HeaderNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ComponentStyle.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>My Simple Header</h1>
+      
+    </header>
+  );
+};
+
+export default Header;
