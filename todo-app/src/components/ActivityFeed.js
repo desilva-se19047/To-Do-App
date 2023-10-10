ad56ad0cf38e7cd45ca8723da3766d68818ca7de
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-10-10 22:22:04 +0530

    modify activityfeed

--- todo-app/src/components/ActivityFeed.js
+++ todo-app/src/components/ActivityFeed.js
@@ -11,21 +11,21 @@ function ActivityFeed() {
           <img src={avatar1} alt="Avatar-1" className="activity-image-1" />        
           <div className="activity-text">
             <p><b>Kushantha Charuka</b> created Contract <a href="#">#00124 need John Beige’s signature</a></p>
-            <p>Sep 16, 2022 at 11:30 AM</p>
+            <p className='time'>Sep 16, 2022 at 11:30 AM</p>
           </div>
         </div>
         <div className="activity-content">
           <img src={avatar2} alt="Avatar-2" className="activity-image-2" />        
           <div className="activity-text">
             <p>Lorem ipsum <b>dolor sit amet</b>, consectetur adipiscing elit. <b>Maecenas</b> pretium neque</p>
-            <p>Sep 16, 2022 at 11:45 AM</p>
+            <p className='time'>Sep 16, 2022 at 11:45 AM</p>
           </div>
         </div>
         <div className="activity-content">
           <img src={avatar2} alt="Avatar-2" className="activity-image-2" />        
           <div className="activity-text">
             <p>Lorem ipsum <b>dolor sit amet</b>, consectetur adipiscing elit. <b>Maecenas</b> pretium neque</p>
-            <p>Sep 16, 2022 at 11:45 AM</p>
+            <p className='time'>Sep 16, 2022 at 11:45 AM</p>
           </div>
         </div>
     </div>
